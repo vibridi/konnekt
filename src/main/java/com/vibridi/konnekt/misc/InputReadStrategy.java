3ba7af0f35d507da0eda52f098b467f4c419765f
gvaccari <tianxiang.vaccari@gmail.com>
2016-11-19 17:18:51 +0100

    Fix project structure

--- src/main/java/com/vibridi/konnekt/misc/InputReadStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.vibridi.konnekt.misc;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-public interface InputReadStrategy {
-	public static final int END_OF_STREAM = -1;
-	public byte[] readInput(InputStream in) throws IOException;
-}
